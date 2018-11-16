ee5438d296d2a9638b1f4a1718aaf0f4f429954a
totonunez <cristobalnunezvera@gmail.com>
2018-11-15 22:35:59 -0300

    generacion aleatoria de datos de sensores

--- GENERACION_DE_DATOS/datosSensores.py
+++ GENERACION_DE_DATOS/datosSensores.py
@@ -1,17 +1,25 @@
+from time import time
 from faker import Faker
-from time import time 
-
+from random import choice, random
 fake = Faker()
 tiempo = time()
 
 print("SENSORES")
-print("insert into choques (id_sensor, nombre, presición) values")
-
+print("insert into choques (id_sensor, nombre, presicion) values")
+x=0
 while (x<10):
-    print("({},'{}','{}','{}'),
-    ".format(x,fake.name(),fake.address(), fake.email(),fake.url()))
+    print("({},'{}','{}'),".format(x,choice(["Sensor De Presion","Sensor De Presion De Sobrealimentacion",
+"Sensor De Masa De Aire","Sensor De Alta Presion","Sonda Lambda",
+"Sensor De Velocidad De Rotacion","Sensor De Presion del Deposito","Transmision De Posicion Del Pedal",
+"Sensor de Angulo","Sensor de Inclinacion","Sensor De Aceleracion",
+"Sensor De Ocupacion De Asiento","Sensor de Magnitud De Giro o Viraje","Sensor De Aceleracion transversal",
+"Sensor de Inclinacion","Sensor De Vuelco","Sensor De velocidad de giro Ruedas",
+"Sensor De lluvia","Sensor Telemetrico"]),"%.2f" %float(random())))
     x = x+1
+    
 print(")")
 
 final = time()- tiempo
-print("Tiempo total en crearse todos los datos es ", final)
+print"Tiempo total en crearse todos los datos es" , final
+
+
