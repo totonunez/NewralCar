7211c079e62835f4a1775ff51878f031ecf3cb8f
lagossully <44487802+lagossully@users.noreply.github.com>
2018-11-19 18:00:16 -0300

    Update datosAutos.py
    puse los generadores de tipo_combustible, tipo_auto, cant_pasajeros, numeroAro

--- GENERACION_DE_DATOS/datosAutos.py
+++ GENERACION_DE_DATOS/datosAutos.py
@@ -5,12 +5,14 @@ from random import random, choice
 
 fake = Faker()
 tiempo = time()
+combustible = ["gasolina98", "gasolina95", "gasolina93", "diesel"]
+tautos = ["Sedán", "Station Wagon", "Doble Cabina", "Hatchback","SUV", "Furgón"]
 print("AUTOS")
-print("insert into choques (id_auto, largo, ancho, alto, peso_neto, peso_max, tipo_combustible, tipo_auto, cant_pasajero, numeroAro) values")
+print("insert into autos (id_auto, largo, ancho, alto, peso_neto, peso_max, tipo_combustible, tipo_auto, cant_pasajero, numeroAro) values")
 
 while (x<10):
     peso_neto = random.uniform(2700, 3860)
-    print("({},'{}' (mm) ,'{}' (mm),'{}' (mm),'{}' (mm), '{}' (mm),'{}' (mm),'{}' (mm),'{}' pasajeros, '{}' ),".format(x,random.uniform(3500, 5000),random.uniform(1700, 1900), random.uniform(1300, 1800),peso_neto + random.uniform(500, 1000)))
+    print("({},'{}' (mm) ,'{}' (mm),'{}' (mm),'{}' (mm), '{}' (mm),'{}' (mm),'{}' (mm),'{}' pasajeros, '{}' ),".format(x,random.uniform(3500, 5000),random.uniform(1700, 1900), random.uniform(1300, 1800),peso_neto + random.uniform(500, 1000), random.choice(combustible), random.choice(tautos), choice(2,4,6), choice(24,26,28) ))
     x = x+1
 print(")")
 
