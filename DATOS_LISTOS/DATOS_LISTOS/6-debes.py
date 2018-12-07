27774e6aa7ea6f8408d51c8b6aceeea02519fa4a
AlonsoGajardo <alonso.gajardo@mail.udp.cl>
2018-12-07 15:00:04 -0300

    Update 6-debes.py

--- DATOS_LISTOS/DATOS_LISTOS/6-debes.py
+++ DATOS_LISTOS/DATOS_LISTOS/6-debes.py
@@ -20,8 +20,12 @@ posts2  = cur.fetchall()
 
 for x in posts:
     for y in  posts2:
+        fechaIncidente = fake.date()
+        fechaPago = fake.date()
+        while fechaIncidente > fechaPago:
+            fechaPago = fake.date()
         sql= ("""insert into debe (id_usuario,id_penalizacion) values""")
-        sql = sql + ("({},{});".format(x[0],y[0]))
+        sql = sql + ("({},{},'{}','{}');".format(x[0],y[0],fechaIncidente,fechaPago))
         cur.execute(sql)
 
 
