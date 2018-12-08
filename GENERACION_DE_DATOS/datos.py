808e0ecb39516fbbda76d3c6b87aae410f7d25b2
180206779 <180206779@Docencia.udp.cl>
2018-12-08 17:31:12 -0300

    ahora si

--- GENERACION_DE_DATOS/datos.py
+++
@@ -1,9 +0,0 @@
-from faker import Faker
-from time import time
-fake = Faker()
-hola = time()
-for i in range(10):
-
-    print(fake.name()," ",fake.email())
-lapsoTiempo = time() - hola
-print(lapsoTiempo)
