dd2b1d39ebf7ff69fcf80e18bdbd611759b89061
lagossully <44487802+lagossully@users.noreply.github.com>
2018-12-08 20:25:33 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -6,28 +6,145 @@ import psycopg2
 conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
 cur = conn.cursor()
 
-
-
-
-
-
-
 @app.route('/')
 @app.route('/index')
 def index():
-	sql ="""
-	select id,nombre from categorias order by nombre
-	"""
-	print sql 
-	cur.execute(sql)
-	categorias  = cur.fetchall()
-	return render_template("index.html",categorias=categorias,nombre="nombre")
+	return render_template("index.html",nombre="nombre")
+
+
+@app.route('/ELIMINAR',methods=['GET', 'POST'])
+def borrar():
+	return render_template("eliminar_inicio.html",nombre="nombre")
+
+
+@app.route('/ELIMINAR_AUTO',methods=['GET', 'POST'])
+def borrarauto():
+	if request.method == 'POST':
+		patente =  request.form['PATENTE']
+		try:
+			sql= """ delete from mediciones where mediciones.patente= %s"""%(patente)
+			cur.execute(sql)
+			sql= """ delete from autos where mediciones.patente = %s""" %(patente)
+			cur.execute(sql)
+			conn.commit()
+			return render_template("eliminacion_ok.html",nombre="nombre")
+		except:
+			return render_template("eliminar_error.html",nombre="nombre")
+	else:
+		return render_template("eliminar_auto.html",nombre="nombre")
+
+
+
+@app.route('/ELIMINAR_CLIENTE',methods=['GET', 'POST'])
+def borrarcliente():
+	if request.method == 'POST':
+		rutcliente = request.form['RUT']
+		try:
+			sql= """ delete from clientes where clientes.rut = %s"""%(rutcliente)
+			cur.execute(sql)
+			sql= """ delete from autos where autos.rut = %s"""%(rutcliente)
+			cur.execute(sql)
+			conn.commit()
+			return render_template("eliminacion_ok.html",nombre="nombre")
+		except:
+			return render_template("eliminar_error.html",nombre="nombre")
+	else:
+		return render_template("eliminar_cliente.html",nombre="nombre")
+
+
+@app.route('/ELIMINAR_DEBE',methods=['GET', 'POST'])
+def borrardebe():
+	if request.method == 'POST':
+		iddebe = request.form['ID']
+		try:
+			sql= """delete from debe where debe.id_penalizacion = %s"""%(iddebe)
+			cur.execute(sql)
+			conn.commit
+			return render_template("eliminacion_ok.html",nombre="nombre")
+		except:
+			return render_template("eliminar_error.html",nombre="nombre")
+	else:
+		return render_template("eliminar_debe.html",nombre="nombre")			
+
+
+@app.route('/ACTUALIZAR', methods=['GET','POST'])
+def actualizar():
+	return render_template('actualizar_inicio.html')
+
+@app.route(/'ACTUALIZAR_DUENO',methods=['GET','POST'])
+def actualizardueno():
+	if request.method == 'POST':
+		rutdueno = resquest.form['RUT']
+		patente2 = request.form['PATENTE']
+		try:
+			sql= """update set auto.montrut = %s, from autos where autos.patente = %s """%(rutdueno,patente2)
+			cur.excecute(sql)
+			conn.commit()
+			return render_template("eliminacion_ok.html",nombre="nombre")
+		except:
+			return render_template("eliminar_error.html",nombre="nombre")
+	else:
+		return render_template("actualizar_dueno.html",nombre="nombre")		
+
+
+@app.route(/'ACTUALIZAR_FECHADEBE',methods=['GET','POST'])
+def actualizardebe():
+	if request.method == 'POST':
+		rutdebe = resquest.form['RUT']
+		fecha = request.form['FECHA_VENC']
+		try:
+			sql= """update set auto.fecha_vencimiento = %s, from autos where autos.patente = %s """%(fecha,rutdebe)
+			cur.excecute(sql)
+			conn.commit()
+			return render_template("eliminacion_ok.html",nombre="nombre")
+		except:
+			return render_template("eliminar_error.html",nombre="nombre")
+	else:
+		return render_template("actualizar_dueno.html",nombre="nombre")	
+
+@app.route(/'ACTUALIZAR_TELEFONO',methods=['GET','POST'])
+def actualizartelefono():
+	if request.method == 'POST':
+		ruttel = resquest.form['RUT']
+		telefono = request.form['TELEFONO']
+		try:
+			sql= """update set clientes.rut = %s, from clientes where clientes.patente = %s """%(ruttel,telefono)
+			cur.excecute(sql)
+			conn.commit()
+			return render_template("eliminacion_ok.html",nombre="nombre")
+		except:
+			return render_template("eliminar_error.html",nombre="nombre")
+	else:
+		return render_template("actualizar_dueno.html",nombre="nombre")	
+
+@app.route(/'CREAR',methods=['GET','POST'])
+def crear():
+	return render_template('crear_inicio.html')
+
+@app.route(/'CREAR_SENSOR',methods=['GET','POST'])
+def crearsensor():
+	if request.method == 'POST':
+		idsensor = resquest.form['ID_SENSOR']
+		fecha = request.form['FECHA_VENC']
+		try:
+			sql= """update set auto.fecha_vencimiento = %s, from autos where autos.patente = %s """%(fecha,rutdebe)
+			cur.excecute(sql)
+			conn.commit()
+			return render_template("eliminacion_ok.html",nombre="nombre")
+		except:
+			return render_template("eliminar_error.html",nombre="nombre")
+	else:
+		return render_template("actualizar_dueno.html",nombre="nombre")	
+
+	
+
+
+
 
 @app.route('/formularios', methods=['GET', 'POST'])
 def formularios():
 	if request.method == 'POST':
 		variable =  request.form['toto']
-		print variable
 		sql = """insert into comentarios (post_id,usuario_id,comentario)
 		values (1,1,'%s')
 		 """%(variable)
@@ -39,133 +156,5 @@ def formularios():
 @app.route('/tablas')
 def tablas():
 	return render_template("tables.html",nombre="nombre")
-
-
-
-@app.route('/post/<post_id>', methods=['GET', 'POST'])
-def post(post_id):
-	if request.method == 'POST':
-		comentario =  request.form['comentarios']
-		print comentario
-		sql = """ insert into comentarios  
-		(post_id,usuario_id,creado,comentario) 
-		values (%s,1,now(),'%s' ) """%(post_id,comentario)
-		cur.execute(sql)
-		conn.commit()
-
-	sql ="""
-	select id,titulo,texto from posts where id = %s
-	"""%post_id
-	print sql
-	cur.execute(sql)
-	post  = cur.fetchone()
-
-	sql ="""
-	select id,nombre from categorias,categorias_posts 
-	where categorias_posts.categoria_id = categorias.id 
-	and post_id = %s 
-	"""%(post_id)
-	print sql
-	cur.execute(sql)
-	categorias  = cur.fetchall()
-
-	sql ="""
-	select comentarios.id,nombre,apellido,comentario
-	
-	from usuarios,comentarios 
-	where comentarios.usuario_id = usuarios.id 
-	and post_id = %s order by id desc
-	"""%(post_id)
-	print sql
-	cur.execute(sql)
-	comentarios  = cur.fetchall()
-	return render_template("post.html",post= post,categorias=categorias,comentarios= comentarios)
-
-
-@app.route('/comentario/<id>', methods=['GET', 'POST'])
-def comentario(id):
-	if request.method == 'POST':
-		comentario =  request.form['comentarios']
-		print comentario
-		sql = """ update comentarios  set comentario = '%s'
-		where id = %s """%(comentario,id)
-		cur.execute(sql)
-		conn.commit()
-
-
-	sql ="""
-	select comentarios.id,nombre,apellido,comentario
-	
-	from usuarios,comentarios 
-	where comentarios.usuario_id = usuarios.id 
-	and comentarios.id = %s order by id desc
-	"""%(id)
-	print sql
-	cur.execute(sql)
-	comentario  = cur.fetchone()
-	return render_template("comentario.html",comentario= comentario) 
-
-	sql ="""
-	select comentarios.id,nombre,apellido,comentario
-	
-	from usuarios,comentarios 
-	where comentarios.usuario_id = usuarios.id 
-	and comentarios.id = %s order by id desc
-	"""%(id)
-	print sql
-	cur.execute(sql)
-	comentario  = cur.fetchone()
-	return render_template("comentario.html",comentario= comentario) 
-
-
-@app.route('/borrar/<id>', methods=['GET', 'POST'])
-def borrarCliente(rut):
-
-
-	sql ="""
-		select patente from autos where rut = %s
-	"""%(rut)
-	cur.execute(sql)
-	patentes = cur.fetchall()
-
-	for x in patente:
-		sql ="""
-		delete from involucrados where patente = %s
-		"""%(x)
-		cur.execute(sql)
-
-	for x in patente:
-		sql ="""
-		delete from mediciones where patente = %s
-		"""%(x)
-		cur.execute(sql)
-
-	sql ="""
-		delete from debe where rut = %s
-		"""%(rut)
-	cur.execute(sql)
-
-	sql ="""
-		delete from autos where rut = %s
-		"""%(rut)
-	cur.execute(sql)
-
-	sql ="""
-		delete from clientes where rut = %s
-		"""%(rut)
-	cur.execute(sql)
-
-	
-	
-
-	
-
-
-
-	cur.execute(sql)
-	conn.commit()
-	
-	return  redirect(request.referrer)
-
 
 
