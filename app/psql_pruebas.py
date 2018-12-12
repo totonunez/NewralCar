313da710b5f5bfece3e8e136713bc68d424c4697
AlonsoGajardo <alonso.gajardo@mail.udp.cl>
2018-12-12 17:58:42 -0300

    Add files via upload

--- app/psql_pruebas.py
+++ app/psql_pruebas.py
@@ -33,29 +33,29 @@ def ELIMINAR_CLIENTE(RUT):
              WHERE clientes.rut='%s' RETURNING *;"""%(RUT)
     print SQL6
     if PATENTE:
-        #try:
-        print cur.execute(SQL2)
-        DATA_BORRADO_INVOLUCRADOS=cur.fetchall()
-        conn.commit()
-        print 'CANTIDAD DE ACCIDENTES ELIMINADOS: ', DATA_BORRADO_INVOLUCRADOS
-        print cur.execute(SQL3)
-        conn.commit()
-        #DATA_BORRADO_MEDICIONES=cur.fetchall()
-        #print 'CANTIDAD DE MEDICIONES ELIMINADAS: ', DATA_BORRADO_MEDICIONES
-        print cur.execute(SQL4)
-        DATA_BORRADO_AUTO=cur.fetchall()
-        conn.commit()
-        print 'CANTIDAD DE AUTOS ELIMINADOS: ', DATA_BORRADO_AUTO
-        print cur.execute(SQL5)
-        DATA_BORRADO_DEBE=cur.fetchall()
-        conn.commit()
-        print 'CANTIDAD DE MULTAS ELIMINADAS: ', DATA_BORRADO_DEBE
-        print cur.execute(SQL6)
-        DATA_BORRADO_CLIENTES=cur.fetchall()
-        conn.commit()
-        #print 'CANTIDAD DE CLIENTES ELIMINADOS: ', DATA_BORRADO_CLIENTES
-        #except:
-         #   print 'ERROR EN EL INTENTO DE ELIMINAR EN INVOLUCRADOS, MEDICIONES, AUTOS, CLIENTES'
+        try:
+            print cur.execute(SQL2)
+            DATA_BORRADO_INVOLUCRADOS=cur.fetchall()
+            conn.commit()
+            print 'CANTIDAD DE ACCIDENTES ELIMINADOS: ', DATA_BORRADO_INVOLUCRADOS
+            print cur.execute(SQL3)
+            conn.commit()
+            #DATA_BORRADO_MEDICIONES=cur.fetchall()
+            #print 'CANTIDAD DE MEDICIONES ELIMINADAS: ', DATA_BORRADO_MEDICIONES
+            print cur.execute(SQL4)
+            DATA_BORRADO_AUTO=cur.fetchall()
+            conn.commit()
+            print 'CANTIDAD DE AUTOS ELIMINADOS: ', DATA_BORRADO_AUTO
+            print cur.execute(SQL5)
+            DATA_BORRADO_DEBE=cur.fetchall()
+            conn.commit()
+            print 'CANTIDAD DE MULTAS ELIMINADAS: ', DATA_BORRADO_DEBE
+            print cur.execute(SQL6)
+            DATA_BORRADO_CLIENTES=cur.fetchall()
+            conn.commit()
+            print 'CANTIDAD DE CLIENTES ELIMINADOS: ', DATA_BORRADO_CLIENTES
+        except:
+            print 'ERROR EN EL INTENTO DE ELIMINAR EN INVOLUCRADOS, MEDICIONES, AUTOS, CLIENTES'
     else:
 
         print 'ERROR CLIENTE NO TIENE PATENTE ASIGNADA' 
@@ -73,11 +73,14 @@ def ELIMINAR_AUTO(PATENTE):
     try:
         cur.execute(SQL2)
         DATA_BORRADO_INVOLUCRADOS=cur.fetchall()
+        conn.commit()
         print 'CANTIDAD DE ACCIDENTES ELIMINADOS: ', DATA_BORRADO_INVOLUCRADOS
         cur.execute(SQL3)
-        DATA_BORRADO_MEDICIONES=cur.fetchall()
-        print 'CANTIDAD DE MEDICIONES ELIMINADAS: ', DATA_BORRADO_MEDICIONES
+        conn.commit()
+        #DATA_BORRADO_MEDICIONES=cur.fetchall()
+        #print 'CANTIDAD DE MEDICIONES ELIMINADAS: ', DATA_BORRADO_MEDICIONES
         cur.execute(SQL4)
+        conn.commit()
         DATA_BORRADO_AUTO=cur.fetchall()
         print 'CANTIDAD DE AUTOS ELIMINADOS: ', DATA_BORRADO_AUTO 
     except:
@@ -90,6 +93,7 @@ def ELIMINAR_MULTA(RUT):
     try:
         cur.execute(SQL5)
         DATA_BORRADO_DEBE=cur.fetchall()
+        conn.commit()
         print 'CANTIDAD DE MULTAS ELIMINADAS: ', DATA_BORRADO_DEBE
     except:
         print 'ERROR EN BORRADO DE MULTAS, RUT INCORRECTO'
@@ -107,11 +111,13 @@ def ACTUALIZAR_DUENO(RUT, PATENTE):
     print SQL2
     cur.execute(SQL)
     DATA_PATENTE_RUT=cur.fetchall()
+    conn.commit()
     if DATA_PATENTE_RUT:
         print DATA_PATENTE_RUT
         try:
             cur.execute(SQL2)
             DATA_ACTUALIZACION_DE_DUENO_AUTO=cur.fetchall()
+            conn.commit()
             print 'SE ACTUALIZAN LA SIGUIENTE CANTIDAD DE ELEMENTOS: ',DATA_ACTUALIZACION_DE_DUENO_AUTO
         except:
             print 'ERROR EN EL INTENTO DE ACTUALIZAR, POSIBLEMENTE EL RUT NUEVO NO EXISTE EN EL SISTEMA'
@@ -132,11 +138,13 @@ def ACTUALIZAR_FECHA_DEBE(RUT, FECHA_NUEVA, ID_MULTA):
     print SQL2
     cur.execute(SQL)
     DATA_CLIENTE_MULTA=cur.fetchall()
+    conn.commit()
     print DATA_CLIENTE_MULTA
     if DATA_CLIENTE_MULTA:
         try:
             cur.execute(SQL2)
             DATA_MULTA_UPDATE=cur.fetchall()
+            conn.commit()
             print 'SE ACTUALIZAN LA SIGUIENTE CANTIDAD DE ELEMENTOS: ',DATA_MULTA_UPDATE
         except:
             print 'ERROR EN EL INTENTO DE ACTUALIZAR FECHAS DE MULTAS'
@@ -155,11 +163,13 @@ def ACTUALIZAR_TELEFONO(RUT, TELEFONO):
     print SQL2
     cur.execute(SQL)
     DATA_TELEFONO_CLIENTE=cur.fetchall()
+    conn.commit()
     if DATA_TELEFONO_CLIENTE:
         print DATA_TELEFONO_CLIENTE
         try:
             cur.execute(SQL2)
             DATA_TELEFONO_UPDATE=cur.fetchall()
+            conn.commit()
             print 'SE ACTUALIZAR LA SIGUIENTE CANTIDAD DE TELEFONOS: ', DATA_TELEFONO_UPDATE
         except:
             print 'ERROR EN EL INTENTO DE ACTUALIZAR EL TELEFONO'
@@ -174,12 +184,14 @@ def INGRESAR_AUTO_NUEVO(PATENTE, RUT, LARGO , ANCHO, ALTO, PESO_NETO, TIPO_COMBU
     try:
         cur.execute(SQL)
         DATA_AUTO_NUEVO=cur.fetchall()
+        conn.commit()
         print 'SE INGRESO CORRECTAMENTE EL AUTO NUEVO', DATA_AUTO_NUEVO
         SQL_PRUEBA="""SELECT *
                       FROM autos
                       WHERE autos.patente='%s';"""%(PATENTE)
         cur.execute(SQL_PRUEBA)
         DATA_COMPROBACION_AUTO_NUEVO=cur.fetchall()
+        conn.commit()
         print 'COMPROBACION DE ENTRADA: ', DATA_COMPROBACION_AUTO_NUEVO
     except:
         print 'ERROR EN EL INGRESO DE UN AUTO NUEVO'
@@ -192,12 +204,14 @@ def INGRESAR_CLIENTE_NUEVO(RUT, DIGITO, NOMBRE, APELLIDO, EMAIL, TELEFONO, URL):
     try:
         cur.execute(SQL)
         DATA_CLIENTE_NUEVO=cur.fetchall()
+        conn.commit()
         print 'SE INGRESO UN CLIENTE NUEVO', DATA_CLIENTE_NUEVO
         SQL_PRUEBA="""SELECT * 
                       FROM clientes
                       WHERE clientes.rut='%s';"""%(RUT)
         cur.execute(SQL_PRUEBA)
         DATA_COMPROBACION_CLIENTE_NUEVO=cur.fetchall()
+        conn.commit()
         print 'COMPROBACION DE ENTRADA: ', DATA_COMPROBACION_CLIENTE_NUEVO
     except:
         print 'ERROR EN EL INGRESO DE UN CLIENTE NUEVO'
